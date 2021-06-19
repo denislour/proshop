b1b4b48935ade50c94a506027bfe80c98d26687e
Lektor <denislour@gmail.com>
2021-06-19 23:00:11 +0700

    [BE] - Apply MongoDB and Colors for logging.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const products = require("./data/products");
+const connectDB = require("./config/db");
+const colors = require("colors");
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -22,5 +26,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
